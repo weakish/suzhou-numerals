59495627d982cb2ad9b475373b4292aa9929fe51
Jang Rush <weakish@gmail.com>
2020-05-16 00:55:13 +0800

    :art: generate js via tsc 3.9.2

--- mod.js
+++ mod.js
@@ -1,13 +1,4 @@
-"use strict";
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
-};
-exports.__esModule = true;
-var hangzhou = [
+const hangzhou = [
     "〇",
     "〡",
     "〢",
@@ -19,16 +10,16 @@ var hangzhou = [
     "〨",
     "〩",
 ];
-var hangzhou_alternative = [
+const hangzhou_alternative = [
     "一",
     "二",
     "三",
 ];
-function decode(s) {
-    return __spreadArrays(s).map(function (c) {
+export function decode(s) {
+    return [...s].map((c) => {
         if (hangzhou_alternative.indexOf(c) === -1) {
             if (hangzhou.indexOf(c) === -1) {
-                throw new TypeError("failed to decode " + c);
+                throw new TypeError(`failed to decode ${c}`);
             }
             else {
                 return hangzhou.indexOf(c).toString();
@@ -39,10 +30,9 @@ function decode(s) {
         }
     }).join("");
 }
-exports.decode = decode;
 function rewrite_with_alternatives(without_alternatives) {
-    without_alternatives.forEach(function (c, i, arr) {
-        var one_two_three = hangzhou.slice(1, 4);
+    without_alternatives.forEach((c, i, arr) => {
+        const one_two_three = hangzhou.slice(1, 4);
         if (one_two_three.includes(c)) {
             if (one_two_three.includes(arr[i - 1])) {
                 arr[i] = hangzhou_alternative[hangzhou.indexOf(c) - 1];
@@ -51,8 +41,7 @@ function rewrite_with_alternatives(without_alternatives) {
     });
     return without_alternatives;
 }
-function encode(n) {
-    return rewrite_with_alternatives(__spreadArrays(n.toString()).map(function (d) { return hangzhou[parseInt(d)]; }))
+export function encode(n) {
+    return rewrite_with_alternatives([...n.toString()].map((d) => hangzhou[parseInt(d)]))
         .join("");
 }
-exports.encode = encode;
