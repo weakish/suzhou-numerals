fedda39a2e3045dbcb7930a0c23833afad72eadd
Jang Rush <weakish@gmail.com>
2022-09-15 07:27:41 +0000

    :zzz: generate js via deno bundle instead of tsc

--- mod.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function decode(s: string): string;
-export declare function encode(n: string): string;
--- mod.js
+++ mod.js
@@ -1,47 +1,63 @@
+// deno-fmt-ignore-file
+// deno-lint-ignore-file
+// This code was bundled using `deno bundle` and it's not recommended to edit it manually
+
 const hangzhou = [
-  "〇",
-  "〡",
-  "〢",
-  "〣",
-  "〤",
-  "〥",
-  "〦",
-  "〧",
-  "〨",
-  "〩",
+    "〇",
+    "〡",
+    "〢",
+    "〣",
+    "〤",
+    "〥",
+    "〦",
+    "〧",
+    "〨",
+    "〩", 
 ];
 const hangzhou_alternative = [
-  "一",
-  "二",
-  "三",
+    "一",
+    "二",
+    "三", 
 ];
-export function decode(s) {
-  return [...s].map((c) => {
-    if (hangzhou_alternative.indexOf(c) === -1) {
-      if (hangzhou.indexOf(c) === -1) {
-        throw new TypeError(`failed to decode ${c}`);
-      } else {
-        return hangzhou.indexOf(c).toString();
-      }
-    } else {
-      return (hangzhou_alternative.indexOf(c) + 1).toString();
-    }
-  }).join("");
+function decodeSequence(seq) {
+    return seq.map((c)=>{
+        if (hangzhou_alternative.includes(c)) {
+            return hangzhou_alternative.indexOf(c) + 1;
+        } else {
+            return hangzhou.indexOf(c);
+        }
+    });
 }
-function rewrite_with_alternatives(without_alternatives) {
-  without_alternatives.forEach((c, i, arr) => {
-    const one_two_three = hangzhou.slice(1, 4);
-    if (one_two_three.includes(c)) {
-      if (one_two_three.includes(arr[i - 1])) {
-        arr[i] = hangzhou_alternative[hangzhou.indexOf(c) - 1];
-      }
-    }
-  });
-  return without_alternatives;
+function decode(s) {
+    const h = [
+        ...s
+    ];
+    const digits = decodeSequence(h);
+    return digits.join("");
 }
-export function encode(n) {
-  return rewrite_with_alternatives(
-    [...n.toString()].map((d) => hangzhou[parseInt(d)]),
-  )
-    .join("");
+function toNumber(s, radix = 10) {
+    return parseInt(decode(s), radix);
 }
+function rewrite_with_alternatives(h) {
+    h.forEach((c, i, arr)=>{
+        const one_two_three = hangzhou.slice(1, 4);
+        if (one_two_three.includes(c)) {
+            if (one_two_three.includes(arr[i - 1])) {
+                arr[i] = hangzhou_alternative[hangzhou.indexOf(c) - 1];
+            }
+        }
+    });
+    return h;
+}
+function encode(n) {
+    return rewrite_with_alternatives([
+        ...n.toString()
+    ].map((d)=>hangzhou[parseInt(d)])).join("");
+}
+function fromNumber(n, radix = 10) {
+    return encode(n.toString(radix));
+}
+export { decode as decode };
+export { toNumber as toNumber };
+export { encode as encode };
+export { fromNumber as fromNumber };
