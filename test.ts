9a1eef70f8b062104c6ed6733eee5fc165b82487
Jang Rush <weakish@gmail.com>
2022-09-15 07:27:41 +0000

    :100: add toNumber & fromNumber tests

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { decode, encode } from "./mod.ts";
+import { decode, encode, fromNumber, toNumber } from "./mod.ts";
 
 const examples: [string, string][] = [
   ["123", "〡二〣"],
@@ -22,6 +22,8 @@ const nonStandardExamples: [string, string][] = [
 
 Deno.test("encode & decode", () => {
   for (const e of examples) {
+    assertEquals(fromNumber(parseInt(e[0])), e[1]);
+    assertEquals(toNumber(e[1]), parseInt(e[0]));
     assertEquals(encode(e[0]), e[1]);
     assertEquals(decode(e[1]), e[0]);
   }
